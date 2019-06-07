046208594e0d294697818bb465950d8a9ecd05f6
Jason <jason.zivkovic@reece.com.au>
2019-06-07 22:31:09 +1000

    started splitting columns

--- src/preprocessing.R
+++ src/preprocessing.R
@@ -2,6 +2,7 @@ library(rvest)
 library(httr)
 library(dplyr)
 library(stringr)
+library(tidyr)
 
 
 all_data <- readRDS("data/afl_games.rds")
@@ -16,3 +17,17 @@ all_data <- all_data %>%
          t2_score = as.numeric(t2_score),
          t1_qtr_scores = str_squish(t1_qtr_scores),
          t2_qtr_scores = str_squish(t2_qtr_scores))
+
+
+
+all_data_cleaned <- all_data %>% 
+ separate(col = match_facts, into= c("date", "info"), sep = "Att:") 
+
+all_data_cleaned <- all_data_cleaned %>% 
+  separate(col = info, into = c("attendance", "venue"), sep = "Venue:")
+
+all_data_cleaned <- all_data_cleaned %>% 
+  separate(date, into = c("date", NA), sep = "\\(") 
+
+# b <- b %>% 
+#   separate(date, into = c("game_date", "start_time"), sep = " ")
