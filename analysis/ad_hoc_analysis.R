9cd7a85ac840c32df94f02b3109d2c816411a674
JaseZiv <jaseziv83@gmail.com>
2019-07-14 13:17:36 +1000

    added testing to see if second venues removed makes a difference to results

--- analysis/ad_hoc_analysis.R
+++ analysis/ad_hoc_analysis.R
@@ -18,3 +18,34 @@ ggsave("plots/med_attendance_team_and_season.png", width = 30, height = 22, unit
 
 team_attendances %>% filter(Team == "St Kilda", home_or_away == "Home") %>% count(venue)
 team_attendances %>% filter(Team == "Essendon", home_or_away == "Home") %>% count(venue)
+
+
+
+
+
+# testing to see if removing Hawthorn's second "home" venue (York Park) makes a difference to the result
+test <- team_attendances %>% 
+  filter(!is.na(last_result),
+         home_or_away == "Home",
+         venue != "York Park") %>%
+  group_by(Team) %>% 
+  mutate(median_home_attendance = median(attendance)) %>% 
+  group_by(Team, last_result, median_home_attendance) %>% 
+  summarise(median_attendances = median(attendance)) %>% 
+  spread(key = last_result, value = median_attendances) %>% 
+  mutate(LIoA = round((Lost - median_home_attendance) / median_home_attendance, 4),
+         WIoA = round((Won - median_home_attendance) / median_home_attendance, 4),
+         OPI = WIoA - LIoA) %>% ungroup()
+
+
+library(kableExtra)
+
+test %>%
+  rename(`Median Home Attendance` = median_home_attendance, `Attendance After Loss` = Lost, `Attendance After Win` = Won) %>% 
+  mutate(
+    LIoA = cell_spec(LIoA, "html", color = ifelse(LIoA < -.1, "red", "grey")),
+    WIoA = cell_spec(WIoA, "html", color = ifelse(WIoA > .11, "red", ifelse( WIoA > .1, "orange", ifelse(WIoA < 0.015, "lightblue", "grey")))),
+    OPI = cell_spec(OPI, "html", color = ifelse(OPI > .2, "red", ifelse(OPI > .16, "orange", ifelse(OPI < 0.01, "lightblue", "grey"))))) %>%
+  select(Team, `Median Home Attendance`, `Attendance After Loss`, `Attendance After Win`, LIoA, WIoA, OPI) %>% 
+  kable(format = "html", escape = F) %>%
+  kable_styling("striped", full_width = F) 
